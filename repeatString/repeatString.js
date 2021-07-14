f96117b46897d0afda4cd60e04802b369714be70
steve <asapcrafter@gmail.com>
2021-07-14 16:37:49 -0700

    hello

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,19 @@
 const repeatString = function() {
-
+    function repeatString(string, times) {
+        if (times < 0) {
+            return 'ERROR';
+        }
+        else if (times == 0) {
+            return '';
+        }
+        else {
+            let totalString = string;
+                for (let i = 1; i < times; i++) {
+                totalString += string;
+                }
+            return totalString;
+        }
+    }
 };
 
 module.exports = repeatString;
