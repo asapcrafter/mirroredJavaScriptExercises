8c528fe8e3888f6e7d072170e99b4e938bfbd10d
Stephen <asapcrafter@gmail.com>
2021-07-16 12:13:01 -0700

    hi

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let array = str.split('');
+    array.reverse('');
+    let reversedArray = array.join('');
+    return reversedArray;
 };
-
 module.exports = reverseString;
