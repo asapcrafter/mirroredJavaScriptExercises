a0f9811f3f85b733858ff2e800eed1155b18370b
Stephen <asapcrafter@gmail.com>
2021-07-19 14:06:57 -0700

    hi

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(numbersArg , ...minus) {
+    let numArray = Array.from(numbersArg);
+    let minusArray = minus;
+    var i = 0;
+    while (i < numArray.length) {
+      if (minusArray.indexOf(numArray[i]) > -1) {
+        numArray.splice(i, 1);
+      } else {
+        ++i;
+      }
+    }
+    return numArray;
+  //  numArray.splice(0, , minusArray); 
+  //  console.log(minusArray);
+   // console.log(numArray);
+  //  console.log("Hello world");
+   // return numArray;
 };
+
+console.log(removeFromArray(['z', 'a', 'b', 'c', 'd', 2], 2, 3, 56, 'a', 'z' ))
+
 
 module.exports = removeFromArray;
