935e2eef3403cf512eaf2c016fdf9b05a940832f
Steve <asapcrafter@gmail.com>
2021-07-27 13:39:53 -0700

    Finished fibonacci function

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(value) {
+    value = Number(value);
+    if (value > 0) {       
+        var values = [1]
+        var firstValue = 0
+        var secondValue = 1
+            for (i = 1; i < value ; i++) {
+                secondValue += firstValue 
+                firstValue = secondValue - firstValue
+                values.push(secondValue);
+            };
+        var finalValue = values[values.length -1];
+        return(finalValue);
+    } else {
+        return("OOPS");
+    }
 };
-
+console.log(fibonacci("5"))
 module.exports = fibonacci;
