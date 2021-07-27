c90ab23ce28bb464aab5e639985615a32056ff32
Steve <asapcrafter@gmail.com>
2021-07-26 23:08:58 -0700

    Finished palindromes exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function(words) {
+    var alphabet = Array.from("qwertyuiopasdfghjklzxcvbnm");
+    var letters = Array.from(words.toLowerCase())
+    var filtered = letters.filter(item => alphabet.includes(item)) ;
+    var reversed = [...filtered].reverse();
+    if (reversed.join('') == filtered.join('')) {
+        return(true)
+    } else {
+        return(false)
+    };
 };
+// console.log(palindromes("Hell123") );
 
 module.exports = palindromes;
+
+/*
+1. Change string to lowercase
+2. Convert string to array
+3. Remove any non-letter array items
+4. Reverse the resulting array
+5. Match and compare
+*/
