db2f995fd6eeaf0fd590d8e59aef4f35b9e347ad
Steve <asapcrafter@gmail.com>
2021-07-26 18:22:52 -0700

    Finished calculator exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,27 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return(a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return(a - b);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0 ) {
+    return(0);
+  } else {
+    var result = arr.reduce((total, num) => total + num);
+    return(result);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  result = arr.reduce((total, num) => total * num);
+  return(result);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	result = Math.pow(a,b);
+  return(result);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x == 0) {
+    return(1);
+  } else {
+    var total = x
+    for (i = x ; i > 1 ; i--) {
+      total *= (i - 1) 
+    }
+    return(total);
+  }
 };
-
 module.exports = {
   add,
   subtract,
